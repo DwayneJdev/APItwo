4b10d5cc1f801b4fcac137d87e0735e8af3e665b
DwayneJdev <88252110+DwayneJdev@users.noreply.github.com>
2021-10-08 21:26:59 -0400

    Add files via upload

---
+++ y/scripts.js
@@ -0,0 +1,55 @@
+
+let search = document.getElementById('btnSearch');
+let output = document.querySelector('.output')
+
+search.addEventListener('click', goodFetch)
+
+let apiKey = "Zp6OCuyLxv4JnA5DMiK6F053mVAjcMBO";
+let url = `https://api.giphy.com/v1/gifs/search`
+ 
+
+
+
+ 
+function goodFetch(g) {
+       g.preventDefault();
+let input = document.getElementById('input').value.trim()
+       let searchUrl= `${url}?&api_key=${apiKey}&rating=pg&q=${input}&limit=15`
+    
+       fetch(searchUrl)
+    .then(function (results) {
+        console.log(results)
+        return results.json();
+    })
+    .then(function (json) {
+        console.log(json);
+        console.log(input)
+          displayGif(json);
+    })
+}
+
+ let displayGif = (content) => {
+     while (output.firstChild) {
+         output.removeChild(output.firstChild)
+     }
+
+     console.log(content)
+     let gifs= content.data
+     gifs.forEach(function(gif) {
+         console.log(gif.images.fixed_width.url)
+         let outputs = document.createElement('img');
+       outputs.src = gif.images.fixed_width.url;
+      output.appendChild(outputs)
+
+     })
+    //  let outputs = document.createElement('img');
+    //   outputs.src = content.data[0].images.downsized.url;
+    //  output.appendChild(outputs)
+    
+ }
+
+// function displayGif(json) {
+//     console.log('displayGif', json)
+//     console.log(json.data[])
+
+// }
